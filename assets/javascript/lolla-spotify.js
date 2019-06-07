67fbfda3e65ae50bead46431d48c4909a9afbfa7
JMDahle <47642064+jmdahle@users.noreply.github.com>
2019-06-07 08:15:54 -0500

    added a test for Spotify

--- assets/javascript/lolla-spotify.js
+++ assets/javascript/lolla-spotify.js
@@ -3,9 +3,9 @@
 
 $("#spotifyLogin").on("click", function() {
     // redirect to log in
-    var url = "https://accounts.spotify.com/authorize?"
-    var clientId = "4e35c64cd250438684608aa0aea8fd7a"
-    var returnUrl = "https://jmdahle.github.io/testSpotify.html"
+    var url = "https://accounts.spotify.com/authorize?";
+    var clientId = "4e35c64cd250438684608aa0aea8fd7a";
+    var returnUrl = "https://jmdahle.github.io/testSpotify.html";
     var scopes = "user-read-private&user-read-email&playlist-modify-public";
     var combinedUrl = url + "client_id" + clientId + "&redirectUrl=" + returnUrl + "&scope=" + scopes + "&response_type=token";
     console.log(combinedUrl);
