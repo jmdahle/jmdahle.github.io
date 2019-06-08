9e4144bd07dffa61302160ed0e53994fa72ef4ee
JMDahle <47642064+jmdahle@users.noreply.github.com>
2019-06-08 16:08:49 -0500

    testing for Spotify hash

--- assets/javascript/lolla-spotify.js
+++ assets/javascript/lolla-spotify.js
@@ -6,9 +6,11 @@ $("#spotifyLogin").on("click", function() {
     var url = "https://accounts.spotify.com/authorize?";
     var clientId = "4e35c64cd250438684608aa0aea8fd7a";
     var returnUrl = "https://jmdahle.github.io/testSpotify.html";
-    var scopes = "user-read-private&user-read-email&playlist-modify-public";
+    returnUrl = location.href;
+    var scopes = "user-read-private%20user-read-email%20playlist-modify-public";
     var combinedUrl = url + "client_id=" + clientId + "&redirect_uri=" + returnUrl + "&scope=" + scopes + "&response_type=token";
     console.log(combinedUrl);
+    $("#linkhere").html("<a href='"+combinedUrl + "'>Login To Spotify</a>");
 })
 
 var qs = location.hash;
