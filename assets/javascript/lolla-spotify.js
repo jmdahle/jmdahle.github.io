e6386431a8b16ca2babf5c4ed1ddf1c8714fdc51
JMDahle <47642064+jmdahle@users.noreply.github.com>
2019-06-08 21:24:34 -0500

    updated Spoitfy test

--- assets/javascript/lolla-spotify.js
+++ assets/javascript/lolla-spotify.js
@@ -2,30 +2,31 @@
 // use to retrieve song information
 
 $("#spotifyLogin").on("click", function() {
-    // redirect to log in
+    // opens the spotify login and returns with a token
     var url = "https://accounts.spotify.com/authorize?";
     var clientId = "4e35c64cd250438684608aa0aea8fd7a";
     var returnUrl = "https://jmdahle.github.io/testSpotify.html";
     returnUrl = location.href;
     var scopes = "user-read-private%20user-read-email%20playlist-modify-public";
     var combinedUrl = url + "client_id=" + clientId + "&redirect_uri=" + returnUrl + "&scope=" + scopes + "&response_type=token";
-    console.log(combinedUrl);
-    $("#linkhere").html("<a href='"+combinedUrl + "'>Login To Spotify</a>");
+    window.open(combinedUrl, "_self");
 })
 
 var responseSpotify = location.hash;
-if (responseSpotify.length > 0) {
+if (responseSpotify.length > 0) { // there is only a "hash" if spotify login returned with an access token
+    // parse the returned "hash" to extract the access token only
     var s = responseSpotify.indexOf("#access_token=") + "#access_token".length + 1;
     var e = responseSpotify.indexOf("&");
     var spotifyToken = responseSpotify.substring(s,e);
     console.log("token",spotifyToken);
-
+    // use the token to get playlists for an artist
     $.ajax({
         url: "https://api.spotify.com/v1/search?q=Ariana+Grande&type=playlist",
         headers: {
             "Authorization": "Bearer " + spotifyToken
         },
         success: function(response) {
+            // dynamically add the playlists to the page
             console.log(response);
             for (var pl = 0; pl < response.playlists.items.length; pl++) {
                 plName = response.playlists.items[pl].name;
@@ -43,6 +44,11 @@ if (responseSpotify.length > 0) {
                 newDiv.append(newP);
                 $("#playlists").append(newDiv);
             }
+            // if any playlist is clicked, open it in a new page
+            $(".playlist").on("click", function() {
+                var url = $(this).attr("data-external-link");
+                window.open(url, "_blank");
+            });
         }
     });
 }
